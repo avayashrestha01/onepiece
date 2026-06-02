847590aee2cfd9eb7df48f8127ac677ecb660e56
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-06-02 16:21:19 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const selector = document.getElementById('theme-selector');
 
+selector.addEventListener('change', (event) => {
+  document.documentElement.setAttribute('data-theme', event.target.value);
+});
