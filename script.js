4afa7cdb572089f12da861513db66cd2a79332f5
avayashrestha01 <90096324+avayashrestha01@users.noreply.github.com>
2026-06-12 12:29:46 +0545

    music and autoRotate

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const selector = document.getElementById('theme-selector');
 selector.addEventListener('change', (event) => {
   document.documentElement.setAttribute('data-theme', event.target.value);
 });
+
+const rotatePics = document.getElementById('rotate');
+
+function rotateAnimation(){
+  if (rotatePics.classList.contains('rotate')){
+    rotatePics.classList.remove('rotate');
+  }
+
+  else{
+    rotatePics.classList.add('rotate');
+  }
+}
+
